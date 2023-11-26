3605e8f80203d2b70e444b059e925a47fe3266b6
IgorMushk <igor.mushketik@gmail.com>
2023-11-26 23:25:54 +0200

    add for previous - src\modules\animals\controllers

--- src/express/routes/animals.js
+++ src/express/routes/animals.js
@@ -1,23 +1,30 @@
 const express = require('express');
+const animalController = require('../../modules/animals/controllers');
+
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.json({message: 'Get animals'});
-});
+// router.get('/', (req,res) => {
+//     res.json({message: 'Get animals'});
+// });
+router.get('/', animalController.getAnimals);
 
-router.get('/:animalId', (req, res) => {
-    //const animalId = req.params.animalId;
-    const {animalId} = req.params;
-    res.json({message: `Get animal ${animalId}`});
-});
+// router.get('/:animalId', (req, res) => {
+//     //const animalId = req.params.animalId;
+//     const {animalId} = req.params;
+//     res.json({message: `Get animal with id ${animalId}`});
+// });
+router.get('/:animalId', animalController.getAnimalById);
 
-router.post('/', (req,res) => {
-    res.json({message: 'Create animal'});
-});
+// router.post('/', (req,res) => {
+//     res.json({message: 'Create animal'});
+// });
+router.post('/', animalController.createAnimal);
 
-router.put('/:animalId', (req,res) => {
-    const {animalId} = req.params;
-    res.json({message: `Update animal with id ${animalId}`});
-});
+
+// router.put('/:animalId', (req,res) => {
+//     const {animalId} = req.params;
+//     res.json({message: `Update animal with id ${animalId}`});
+// });
+router.put('/:animalId', animalController.updateAnimal);
 
 module.exports = router;
