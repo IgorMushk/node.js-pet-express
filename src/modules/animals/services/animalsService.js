9fb5cdaed2cd50d9942dc9b95beca718ff541912
IgorMushk <igor.mushketik@gmail.com>
2023-11-27 17:52:12 +0200

    middleware error handler

--- src/modules/animals/services/animalsService.js
+++ src/modules/animals/services/animalsService.js
@@ -1,24 +1,30 @@
+const HttpError = require("../../common/models/HttpError");
 const Animal = require("../models/animal");
 const animalRepository = require("../repositories/animalsREpository");
 
 class AnimalsService {
     constructor(animalRepositoty) {
-        this.animalRepositoty = animalRepositoty;
+        this.animalRepository = animalRepository;
     }
 
     // описываем интерфейс
     async getAll() {
        // a lot of logic 
-       return await this.animalRepositoty.findAll();
+       return await this.animalRepository.findAll();
     }
 
     async getOneById(id) {
-        return await this.animalRepositoty.findOneById(id);
-    }
+        //return await this.animalRepository.findOneById(id);
+        const animal = await this.animalRepository.findOneById(id);
+        if (!animal) {
+          throw new HttpError(404, 'Animal is not found');
+        }   
+        return animal;
+        }
 
     async create(payload) {
         const animal = new Animal(payload);
-        return await this.animalRepositoty.create(animal);
+        return await this.animalRepository.create(animal);
     }
     
     updateById(id, payload) {}
